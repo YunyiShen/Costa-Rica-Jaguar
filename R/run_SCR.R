a43191733a2ec4f104c9475d21a8474c4fd893af
YunyiShen <yshen99@wisc.edu>
2022-12-18 17:33:20 -0500

    test workflow

--- R/run_SCR.R
+++ R/run_SCR.R
@@ -7,6 +7,6 @@ load("./clean_data/scr_stan_data.rda")
 
 m_init <- stan_model("./stan/scr0-poisson-integrated.stan")
 m_fit <- sampling(m_init,  data = scr_stan_data)
-traceplot(m_fit, pars = c("alpha1", "alpha0", "N"))
+#traceplot(m_fit, pars = c("alpha1", "alpha0", "N"))
 
 save(m_fit, file="./res/scr_stan_fit.rda")
