a97e0d8e77790c369f988e28d87336da99bf950c
YunyiShen <yshen99@wisc.edu>
2023-01-20 08:50:24 -0500

    minor changes, add readme

--- R/get_density_plot_js.R
+++ R/get_density_plot_js.R
@@ -7,6 +7,8 @@ source("./R/utils.R")
 load("./res/js_stan_fit.rda")
 load("./clean_data/grid_objs_data.rda")
 load("./clean_data/js_stan_data.rda")
+park_boundry <- st_read("./data/mask/Corcovado/Corcovado.shp") |> 
+  st_transform(crs = CRS("+proj=utm +zone=17"))
 
 years <- 1:4+2017
 
@@ -34,9 +36,16 @@ png("./res/Figs/js_den_est.png", width = 6 * 2, height = 4 * 2, units = "in",res
 par(mfrow = c(2,2))
 for(i in 1:4){
   density_est[[i]] <- JSdensity(s,z,JS_stan_data$grid_pts,i,
-                    nx = 40, ny = 40, main = i+2017)
+                    nx = 40, ny = 40, main = i+2017, 
+                    Xl = 0.999*198500/grid_objs$scaling, Xu = 1.001*max(JS_stan_data$grid_pts[,1]),
+                    Yl = 0.999*min(JS_stan_data$grid_pts[,2]), 
+                    Yu = 1.001*max(JS_stan_data$grid_pts[,2]),
+                    )
   points(grid_objs$traplocs, pch = 2)
+  points(JS_stan_data$grid_pts, pch = 20, 
+         col = adjustcolor("red", alpha.f = 0.2), cex = 0.3)
+  plot(as.data.frame(park_boundry$geometry)/grid_objs$scaling, add = T)
 }
 dev.off()
 
-plot(m_fit, pars = "beta_env")
+plot(m_fit, pars = c("psi","gamma","phi","p0","alpha1"),plotfun = "trace")
---
+++ README.md
@@ -0,0 +1,7 @@
+# Status of Jaguar in Corcovado National Park, Costa Rica
+
+This repo contains code to run capture-recapture model to determine the status of jaguars in Corcovado National Park, Costa Rica. 
+
+We implemented a (spatially-explicit) Jolly-Seber model with habitat selection in Stan. (See the `stan` folder). Since Stan does not allow discrete parameters, the state of the individual was manually marginalized during posterior sampling via a forward algorithm and the sample back using forward filtering backward sampling (FFBS) algorithm. One can use `make` to run part of all of the work flow
+
+![](https://raw.githubusercontent.com/YunyiShen/Costa-Rica-Jaguar/master/res/Figs/js_den_est.png)
