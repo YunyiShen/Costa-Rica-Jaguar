1c7fac42c5805c587ff4d28aa505f0430853f894
YunyiShen <yshen99@wisc.edu>
2023-02-06 18:15:08 -0500

    running code for locked location

--- R/run_JS_lock.R
+++ R/run_JS_lock.R
@@ -1,5 +1,5 @@
 library(rstan)
-options(mc.cores = parallel::detectCores()/2)
+options(mc.cores = 4)
 rstan_options(auto_write = TRUE)
 
 load("./clean_data/js_stan_data.rda")
@@ -12,9 +12,9 @@ envX_dyna <- rowMeans(envX_dyna)
 JS_stan_data$envX <- cbind(envX_dyna, envX_static)
 
 set.seed(42)
-m_fit <- sampling(m_init,  data = JS_stan_data,chains = 1, iter = 10, 
+m_fit <- sampling(m_init,  data = JS_stan_data,chains = 4, iter = 1000, 
                   init = function() list(gamma = 0.2, psi = 0.15, 
                                          phi = 0.85, alpha1 = .7, 
                                          p0 = .03, beta = rep(0,JS_stan_data$n_env)), 
                   verbose = TRUE)
-
+save(m_fit, file="./res/js_lock_stan_fit.rda")
