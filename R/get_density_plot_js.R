dc4ab953407442d4d0b73ee4cbb8e805bc675427
YunyiShen <yshen99@wisc.edu>
2023-02-10 20:01:05 -0500

    get good things

--- R/get_density_plot_js.R
+++ R/get_density_plot_js.R
@@ -3,10 +3,10 @@ library(sf)
 library(sp)
 library(ggplot2)
 library(reshape)
-library(fileds)
+library(fields)
 source("./R/utils.R")
 
-load("./res/js_lock_null_stan_fit.rda")
+load("./res/js_lock_prey_stan_fit.rda")
 load("./clean_data/grid_objs_data.rda")
 load("./clean_data/js_stan_data.rda")
 park_boundry <- st_read("./data/mask/Corcovado/Corcovado.shp") |> 
@@ -29,13 +29,13 @@ ggplot(NN, aes(x=variable, y=value)) +
   geom_point(data = NN_mean) + 
   geom_line(aes(group = 1),data = NN_mean)
 
-ggplot2::ggsave("./res/Figs/js_pop_est.png", width = 6, height = 4, unit = "in")
+ggplot2::ggsave("./res/Figs/js_prey_pop_est.png", width = 6, height = 4, unit = "in")
 
 
 s <- rstan::extract(m_fit, c("s"))$s
 
 density_est <- list()
-png("./res/Figs/js_null_den_est.png", width = 6 * 2, height = 4 * 2, units = "in",res = 500)
+png("./res/Figs/js_prey_den_est.png", width = 6 * 2, height = 4 * 2, units = "in",res = 500)
 par(mfrow = c(2,2),mar = c(2.5,2.5,1,.5), mgp = c(1.5, 0.5, 0))
 for(i in 1:4){
   density_est[[i]] <- JSdensity(s,z,JS_stan_data$grid_pts,i,TRUE,
@@ -49,10 +49,10 @@ for(i in 1:4){
   fields::image.plot(density_est[[i]]$grid$xg, 
             density_est[[i]]$grid$yg, 
             density_est[[i]]$Dn, 
-            zlim = c(0.1,20), xlab = "", ylab = "", 
+            zlim = c(0.,30), xlab = "", ylab = "", 
             main = i+2017,
             col = gray.colors(20, start = 0., 
-                    end = 0.9, gamma = 1.1, rev = TRUE))
+                    end = 0.9, gamma = .8, rev = TRUE))
   points(grid_objs$traplocs[rowSums(JS_stan_data$deploy[i,,])>0,], pch = 2)
   for(j in 1:13){ # 13 seen individuals
     points(grid_objs$traplocs[rowSums(JS_stan_data$y [j,,,i])>0,], pch = j+2, col = "blue")
@@ -68,9 +68,9 @@ plot(m_fit, pars = c("beta_env"),plotfun = "hist")
 plot(m_fit, pars = c("psi","gamma","phi","p0","alpha1"),plotfun = "trace")
 pairs(m_fit, pars = c("beta_env"))
 
-png("./res/Figs/js_act_center.png", width = 10 * 2, height = 4 * 2, units = "in",res = 500)
+png("./res/Figs/js_prey_act_center.png", width = 10 * 2, height = 4 * 2, units = "in",res = 500)
 par(mfrow = c(3,6))
-year <- 3
+year <- 4
 for(i in 1:18){
   JSdensity(s,z,JS_stan_data$grid_pts,year,TRUE,
             nx = 46, ny = 37, main = paste(year+2017,"inid:", i), 
